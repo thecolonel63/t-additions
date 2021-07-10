195a5c08bca07dd252a1d0f85c15b1e4f4fec161
Buck Turk <37983248+thecolonel63@users.noreply.github.com>
2021-07-10 18:16:08 -0500

    Delete Commands.java

--- src/main/java/com/yahoo/turkbuck/tadditions/command/Commands.java
+++
@@ -1,9 +0,0 @@
-package com.yahoo.turkbuck.tadditions.command;
-
-public class Commands {
-    public static void registerCommands() {
-        tgivecommand.register();
-        tmodifycommand.register();
-        treplaceitem.register();
-    }
-}
