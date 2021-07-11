c09dd8974779580a394f7d06f522100519ac00e6
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 14:26:03 -0500

    Disabled /tuuid, as it is still in development, and completely removed /tspectp, as it was useless. It will probably be brought back.

--- src/main/java/com/yahoo/turkbuck/tadditions/TAdditions.java
+++ src/main/java/com/yahoo/turkbuck/tadditions/TAdditions.java
@@ -4,7 +4,13 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.yahoo.turkbuck.tadditions.command.*;
 import com.yahoo.turkbuck.tadditions.command.helpers.CommandManager;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.realms.dto.PlayerInfo;
 import net.minecraft.server.command.ServerCommandSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class TAdditions implements ModInitializer {
 
@@ -14,26 +20,9 @@ public class TAdditions implements ModInitializer {
 
     public static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
         CommandManager.clearClientSideCommands();
-//        tspectpcommand.register(dispatcher);
         tgivecommand.register(dispatcher);
         tmodifycommand.register(dispatcher);
         treplaceitemcommand.register(dispatcher);
-        tuuidcommand.register(dispatcher);
-    }
-
-    public static boolean isUUID(String s) {
-        if (s.length() == 36) {
-            if (s.substring(8, 9).contains("-") && s.substring(13, 14).contains("-") && s.substring(18, 19).contains("-") && s.substring(23, 24).contains("-")) {
-                String part1 = s.substring(0, 7);
-                String part2 = s.substring(9, 12);
-                String part3 = s.substring(14, 17);
-                String part4 = s.substring(19, 22);
-                String part5 = s.substring(24, 36);
-                String dashesRemoved = part1+part2+part3+part4+part5;
-                if (dashesRemoved.matches("-?[0-9a-f]+")) {
-                    return true;
-                } else return false;
-            } else return false;
-        } else return false;
+//        tuuidcommand.register(dispatcher);
     }
 }
