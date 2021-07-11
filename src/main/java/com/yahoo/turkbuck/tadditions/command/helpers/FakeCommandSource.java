a562775b54173fac488bbd4be3a520d3b3ff9a48
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 14:49:36 -0500

    Re-Added FakeCommandSource

---
+++ src/main/java/com/yahoo/turkbuck/tadditions/command/helpers/FakeCommandSource.java
@@ -0,0 +1,21 @@
+package com.yahoo.turkbuck.tadditions.command.helpers;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class FakeCommandSource extends ServerCommandSource {
+    public FakeCommandSource(ClientPlayerEntity player) {
+        super(player, player.getPos(), player.getRotationClient(), null, 0, player.getEntityName(), player.getName(), null, player);
+    }
+
+    @Override
+    public Collection<String> getPlayerNames() {
+        return MinecraftClient.getInstance().getNetworkHandler().getPlayerList()
+                .stream().map(e -> e.getProfile().getName()).collect(Collectors.toList());
+    }
+}
