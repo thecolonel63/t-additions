b2c91c71e337dc353ae6a5f69389ba7dd896c286
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 13:32:35 -0500

    Modified to use new command processor.

--- src/main/java/com/yahoo/turkbuck/tadditions/TAdditions.java
+++ src/main/java/com/yahoo/turkbuck/tadditions/TAdditions.java
@@ -1,28 +1,15 @@
 package com.yahoo.turkbuck.tadditions;
 
-import com.mojang.brigadier.CommandDispatcher;
-import com.yahoo.turkbuck.tadditions.command.*;
-import com.yahoo.turkbuck.tadditions.command.helpers.CommandManager;
+import com.yahoo.turkbuck.tadditions.command.Commands;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.realms.dto.PlayerInfo;
-import net.minecraft.server.command.ServerCommandSource;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.Charset;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class TAdditions implements ModInitializer {
 
     @Override
     public void onInitialize() {
-    }
-
-    public static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
-        CommandManager.clearClientSideCommands();
-        tgivecommand.register(dispatcher);
-        tmodifycommand.register(dispatcher);
-        treplaceitemcommand.register(dispatcher);
-//        tuuidcommand.register(dispatcher);
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            Commands.registerCommands();
+        });
     }
 }
