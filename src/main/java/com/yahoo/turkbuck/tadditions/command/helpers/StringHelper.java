9f5e7fd2ea5342f5622c86765b5af2c1030d849c
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 13:29:17 -0500

    Updated StringHelper

--- src/main/java/com/yahoo/turkbuck/tadditions/command/helpers/StringHelper.java
+++ src/main/java/com/yahoo/turkbuck/tadditions/command/helpers/StringHelper.java
@@ -1,7 +1,7 @@
 package com.yahoo.turkbuck.tadditions.command.helpers;
 
 public class StringHelper {
-    public static boolean isUUID(String s) {
+    public static boolean isHexadecimalUUID(String s) {
         if (s.length() == 36) {
             if (s.substring(8, 9).contains("-") && s.substring(13, 14).contains("-") && s.substring(18, 19).contains("-") && s.substring(23, 24).contains("-")) {
                 String part1 = s.substring(0, 7);
@@ -9,11 +9,31 @@ public class StringHelper {
                 String part3 = s.substring(14, 17);
                 String part4 = s.substring(19, 22);
                 String part5 = s.substring(24, 36);
-                String dashesRemoved = part1+part2+part3+part4+part5;
-                if (dashesRemoved.matches("-?[0-9a-f]+")) {
-                    return true;
-                } else return false;
-            } else return false;
-        } else return false;
+                String dashesRemoved = part1 + part2 + part3 + part4 + part5;
+                return dashesRemoved.matches("-?[0-9a-f]+");
+            }
+        }
+        return false;
+    }
+
+    public static boolean isIntArrayUUID(String s) {
+        if ((s.startsWith("[I;")) && (s.charAt(s.length() - 1) == ']')) {
+            long count = s.substring(3, s.length() - 1).chars().filter(ch -> ch == ',').count();
+            if (count == 3) {
+                String[] parts = s.substring(3, s.length() - 1).split(",");
+                if (parts.length == 4) {
+                    int partNumericalCheck = 0;
+                    for (int i = 0; i < 4; i++) {
+                        try {
+                            int parsedInt = Integer.parseInt(parts[i]);
+                        } catch (NumberFormatException e) {
+                            partNumericalCheck++;
+                        }
+                    }
+                    return partNumericalCheck == 0;
+                }
+            }
+        }
+        return false;
     }
 }
