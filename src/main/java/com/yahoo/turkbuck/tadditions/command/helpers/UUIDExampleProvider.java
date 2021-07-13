7679d8b35fee9d15adad4b0f2df0ba0cc5a2606e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-13 13:30:15 -0500

    Added UUIDExampleProvider

---
+++ src/main/java/com/yahoo/turkbuck/tadditions/command/helpers/UUIDExampleProvider.java
@@ -0,0 +1,17 @@
+package com.yahoo.turkbuck.tadditions.command.helpers;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+
+import java.util.concurrent.CompletableFuture;
+
+public class UUIDExampleProvider implements SuggestionProvider<FabricClientCommandSource> {
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<FabricClientCommandSource> context, SuggestionsBuilder builder) {
+        builder.suggest("00000001-0000-0002-0000-000300000004");
+        builder.suggest("[I;1,2,3,4]");
+        return builder.buildFuture();
+    }
+}
